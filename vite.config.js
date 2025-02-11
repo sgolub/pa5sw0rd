5b5504270f4d378306a9d06758de1c6aa0cb6762
Sergey Golub <sergeyglb@outlook.com>
2025-02-12 00:01:01 +0100

    feat: update TypeScript configuration and restructure Vite build for improved module handling

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,25 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
-    dts({ include: ['src/**/*.ts', 'src/**/*.dts'], rollupTypes: true }),
+    dts({ include: ['src/**/*.ts', 'src/**/*.dts'] }),
     react({ include: ['demo/**/*.tsx'] })
   ],
   build: {
+    rollupOptions: {
+      external: ['node:crypto'],
+    },
     lib: {
       name: 'Pa5sW0rd',
-      entry: resolve(__dirname, 'src/index.ts'),
-      fileName: 'pa5sw0rd'
+      entry: {
+        'index': resolve(__dirname, 'src/index.ts'),
+        'browser': resolve(__dirname, 'src/browser.ts'),
+        'password': resolve(__dirname, 'src/password/index.ts'),
+        'pin': resolve(__dirname, 'src/pin/index.ts'),
+        'passphrase': resolve(__dirname, 'src/passphrase/index.ts'),
+        'dictionary': resolve(__dirname, 'src/dictionary/index.ts')
+      },
     },
-    copyPublicDir: false
+    copyPublicDir: false,
   },
   server: {
     open: '/demo/index.html'
