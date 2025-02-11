1fcbbf046e510668639f6b53dc9d26061b253a75
Sergey Golub <sergeyglb@outlook.com>
2025-02-12 00:01:52 +0100

    feat: implement custom crypto handling for browser and Node.js environments

--- src/common/crypto.ts
+++ src/common/crypto.ts
@@ -1,8 +1,31 @@
 const isBrowser: boolean =
   typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
-const crypto: Crypto = isBrowser
-  ? window.crypto
-  : (require('node:crypto').webcrypto as Crypto);
+var customCrypto: Crypto;
+
+function crypto(): Crypto;
+function crypto(crypto: Crypto): void;
+function crypto(crypto?: Crypto): Crypto | void {
+  if (!isBrowser && crypto) {
+    customCrypto = crypto;
+    return;
+  }
+
+  if (isBrowser && crypto) {
+    console.warn(
+      'Custom crypto object is not supported in browser environment',
+    );
+  }
+
+  if (isBrowser) {
+    return window.crypto;
+  }
+
+  if (customCrypto) {
+    return customCrypto;
+  }
+
+  throw new Error('Crypto object is not available');
+}
 
 export default crypto;
